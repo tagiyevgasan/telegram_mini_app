d39ae1feacd609f8a11dea83303d056b1ccdb78b
Taghiyev Gasan <tagiyevgasan@gmail.com>
2025-02-27 18:53:49 +0000

    updt3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let tg = window.Telegram.WebApp;
 let buy = document.getElementById("buy");
 let order = document.getElementById("order");
+tg.expand();
 
 
 buy.addEventListener("click", () => {
@@ -11,5 +12,36 @@ buy.addEventListener("click", () => {
 
 
 order.addEventListener("click", () => {
+
+    document.getElementById("error").innerText = '';
+    let name = document.getElementById("user_name").value;
+    let email = document.getElementById("user_email").value;
+    let phone = document.getElementById("user_phone").value;
+
+    if(name.length <5) {
+
+        document.getElementById("error").innerText = 'Oshibka v imeni';
+        return;
+    }
+    if(email.length <5) {
+
+        document.getElementById("error").innerText = 'Oshibka v emaili';
+        return;
+    }
+    if(phone.length <5) {
+
+        document.getElementById("error").innerText = 'Oshibka v nomere';
+        return;
+    }
+
+
+    let data = {
+        name: name,
+        email: email,
+        phone: phone
+    }
+
+    tg.sendData(JSON.stringify(data));
+
     tg.close();
 });
