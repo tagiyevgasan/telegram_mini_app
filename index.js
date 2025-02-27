7390e7fd191da24a6a500425f1dba449d1e35870
Taghiyev Gasan <tagiyevgasan@gmail.com>
2025-02-27 18:03:09 +0000

    update

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function addNumbers(a, b) {
-    return a + b;
-}
+let tg = window.Telegram.WebApp;
+let buy = document.getElementById("buy");
+let order = document.getElementById("order");
+
+
+buy.addEventListener("click", () => {
+    document.getElementById("main").style.display = "none";
+    document.getElementById("form").style.display = "block";
+    document.getElementById("user_name").value = tg.initDataUnsafe.user.first_name + ' ' + tg.initDataUnsafe.user.last_name;
+});
+
+
+order.addEventListener("click", () => {
+    tg.close();
+});
